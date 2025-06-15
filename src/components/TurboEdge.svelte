ec4ae39efdf6dffe4f66674257de8455050507d7
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-06-15 16:19:49 +0100

    start remaking the ui like auto-spammer used to be

---
+++ src/components/TurboEdge.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { type EdgeProps, getBezierPath } from "@xyflow/svelte";
+
+    let {
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition,
+        markerEnd,
+        id,
+    }: EdgeProps = $props();
+
+    let [edgePath] = $derived.by(() => {
+        const xEqual = sourceX === targetX;
+        const yEqual = sourceY === targetY;
+        return getBezierPath({
+            // we need this little hack in order to display the gradient for a straight line
+            sourceX: xEqual ? sourceX + 0.0001 : sourceX,
+            sourceY: yEqual ? sourceY + 0.0001 : sourceY,
+            sourcePosition,
+            targetX,
+            targetY,
+            targetPosition,
+        });
+    });
+</script>
+
+<path {id} class="svelte-flow__edge-path" d={edgePath} marker-end={markerEnd} />
---
+++ src/components/TurboNode.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { Handle, Position, type NodeProps } from "@xyflow/svelte";
+
+    let { data }: NodeProps = $props();
+</script>
+
+<div class="cloud gradient">
+    <div>Cloud</div>
+</div>
+<div class="wrapper gradient">
+    <div class="inner">
+        <div class="body">
+            {#if data.icon}
+                <div class="icon">File</div>
+            {/if}
+            <div>
+                <div class="title">{data.title}</div>
+                {#if data.subtitle}
+                    <div class="subtitle">{data.subtitle}</div>
+                {/if}
+            </div>
+        </div>
+        <Handle type="target" position={Position.Left} />
+        <Handle type="source" position={Position.Right} />
+    </div>
+</div>
